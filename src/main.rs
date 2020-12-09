e7f143c9c1b15dbd0ff1ad95d063d13608e3be09
rhinocerose <ashar.k.latif@gmail.com>
2020-12-08 20:25:21 -0500

    Simplified sync

--- src/main.rs
+++ src/main.rs
@@ -5,42 +5,24 @@ mod configs;
 
 use canparse::pgn::PgnLibrary;
 use configs::Overview;
-use futures_util::stream::StreamExt;
-use tokio_socketcan::{CANSocket, Error};
 use std::fs;
-// use socketcan::CANSocket;
+use socketcan::CANSocket;
 
 const DBC_FILE: &str        = "./data/anz.dbc";
 const CAN_CHANNEL: &str     = "vcan0";
 const CONFIG_FILE: &str     = "./data/config.yaml";
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
+fn main() {
 
     let mut system_values   = Overview::new();
-    let lib                 = PgnLibrary::from_dbc_file(DBC_FILE)?;
-    let mut socket_rx       = CANSocket::open(CAN_CHANNEL)?;
+    let lib                 = PgnLibrary::from_dbc_file(DBC_FILE).expect("no such file");
+    let mut socket_rx       = CANSocket::open(CAN_CHANNEL).expect("no such file");
 
     let file_parse: String = fs::read_to_string(CONFIG_FILE).expect("no such file");
     system_values.add_nodes(&file_parse);
 
-    while let Some(Ok(frame)) = socket_rx.next().await {
-        system_values.parse_frame(&lib, &frame).expect("Unable to read frame");
+    loop {
+
     }
-    Ok(())
+
 }
-
-// fn main() {
-
-//         let mut system_values   = Overview::new();
-//         let lib                 = PgnLibrary::from_dbc_file(DBC_FILE).expect("no such file");
-//         let mut socket_rx       = CANSocket::open(CAN_CHANNEL).expect("no such file");
-
-//         let file_parse: String = fs::read_to_string(CONFIG_FILE).expect("no such file");
-//         system_values.add_nodes(&file_parse);
-
-//         loop {
-
-//         }
-
-// }
